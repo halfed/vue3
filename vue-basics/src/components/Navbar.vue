f2a078267c7009a83f769fc13b059ae08012e936
halfed <1half.ed@gmail.com>
2024-06-23 14:24:29 -0600

    Creating new Vue3 blog post demo

---
+++ vue-basics/src/components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="navbar">
+        <div class="navbar-end">
+            <div class="buttons">
+                <RouterLink to="/posts/new" class="button">New Posts</RouterLink>
+            </div>
+        </div>
+    </div>
+</template>
